a9e95f9aaca37f4c61e5ba9c24b14ba078a705ae
juyeon <juyeonh@rainist.com>
2020-06-21 14:50:43 +0900

    Add CreatePollForm component for adding new polls

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
+	import CreatePollForm from './components/CreatePollForm.svelte';
 	import Tabs from './shared/Tabs.svelte';
 
 	// tabs
@@ -18,7 +19,7 @@
 	{#if activeItem === items[0]}
 		<p>Poll List component goes here</p>
 	{:else if activeItem === items[1]}
-		<p>new Poll form component goes here</p>
+		<CreatePollForm />
 	{/if}
 </main>
 <Footer />
---
+++ src/components/CreatePollForm.svelte
@@ -0,0 +1,46 @@
+<script>
+  let fields = {
+    question: '',
+    answerA: '',
+    answerB: '',
+  };
+
+  const handleSubmit = () => {
+    console.log(fields);
+  }
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <div class='form-field'>
+    <label for='question'>Poll Question:</label>
+    <input type='text' id='question' bind:value={fields.question} />
+  </div>
+  <div class='form-field'>
+    <label for='answerA'>Answer A:</label>
+    <input type='text' id='answerA' bind:value={fields.answerA} />
+  </div>
+  <div class='form-field'>
+    <label for='answerB'>Answer B:</label>
+    <input type='text' id='answerB' bind:value={fields.answerB} />
+  </div>
+  <button>Add Poll</button>
+</form>
+
+<style>
+  form {
+    width: 400px;
+    margin: 0 auto;
+    text-align: center;
+  }
+  .form-field {
+    margin: 18px auto;
+  }
+  input {
+    width: 100%;
+    border-radius: 6px;
+  }
+  label {
+    margin: 10px auto;
+    text-align: left;
+  }
+</style>
